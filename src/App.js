9cb70bfd70830b1af793286f45a13a5e808beec2
CarlosCubNed <kuva5008@gmail.com>
2022-09-03 08:37:44 +0200

    dynamic values

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-
+import { actions } from './store/index'
 
 function App() {
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
 
   const increment = () => {
-    dispatch({ type: "INC" })
+    dispatch(actions.increment())
   }
 
   const decrement = () => {
-    dispatch({ type: "DEC" })
+    dispatch(actions.decrement())
   }
 
   const addBy = () => {
-    dispatch({ type: "ADD", payload: 10 })
+    dispatch(actions.addby(10))
   }
   return (
     <div>
