12d9068af93395dcc1da6ebf1c2476dcdbcae11e
CarlosCubNed <kuva5008@gmail.com>
2022-09-03 08:06:36 +0200

    adding buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 
 function App() {
-  return (
-    <div className="App">
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
+  const increment = () => {
+    dispatch({ type: "INC" })
+  }
+
+  const decrement = () => {
+    dispatch({ type: "DEC" })
+  }
+
+  const addBy = () => {
+    dispatch({ type: "ADD", payload: 10 })
+  }
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={addBy}>Add by 10</button>
     </div>
   );
 }
